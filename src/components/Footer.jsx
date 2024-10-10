0bf266d43c5784768a68419380b7f0d65c0b3d3b
dekaroyanto <dekaroy05@gmail.com>
2024-10-10 12:54:50 +0700

    [DEPLOY]

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,13 +14,13 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#">
+              <a href="https://www.linkedin.com/in/deka-royanto/">
                 <img src={navIcon1} alt="Icon" />
               </a>
-              <a href="#">
+              <a href="https://www.facebook.com/deka.royanto.1?mibextid=LQQJ4d">
                 <img src={navIcon2} alt="Icon" />
               </a>
-              <a href="#">
+              <a href="https://www.instagram.com/dekaroyanto/">
                 <img src={navIcon3} alt="Icon" />
               </a>
             </div>
